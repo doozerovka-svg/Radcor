54701693a6499e058a3a45949665419169de762e
doozerovka-svg <doozerovka@gmail.com>
2026-08-08 23:29:52 +0300

    feat: export updated transport scraped data into products.json (v40.0)

--- tests/test_r4_page_integrity.js
+++ tests/test_r4_page_integrity.js
@@ -2,7 +2,7 @@ const fs = require('fs');
 const path = require('path');
 const vm = require('vm');
 
-const EXPECTED_VERSION = '?v=39.0';
+const EXPECTED_VERSION = '?v=40.0';
 const ROOT_DIR = path.resolve(__dirname, '..');
 
 const HTML_PAGES = [
